097cff80ad8469878ba7634f53433fee876dd1d9
Rhnw21 <166722865+Rhnw21@users.noreply.github.com>
2024-08-01 21:22:45 +0700

    update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const startSock = async () => {
 	
 	setInterval(() => {
 	  const d = new Date()
-	  const currentTime = `${d.getHours()}:${d.getMinutes()}
+	  const currentTime = `${d.getHours()}:${d.getMinutes()}`
 	  if (currentTime === '7:30' || currentTime === '11:30' || currentTime === '16:30') {
 	    const chatId = '120363045832731477@g.us'
 	    sock.sendMessage(chatId, {
